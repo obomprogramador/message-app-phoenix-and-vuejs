054cdbca31acf32c6cc463200996c8212eac333c
Carlos H.F.S <obomprogramador@gmail.com>
2026-07-24 12:46:27 -0300

    Add seed function to Release module and call it on deploy

--- backend/lib/message_app/release.ex
+++ backend/lib/message_app/release.ex
@@ -20,6 +20,11 @@ defmodule MessageApp.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def seed do
+    load_app()
+    Code.eval_file("priv/repo/seeds.exs")
+  end
+
   defp repos do
     Application.fetch_env!(app(), :ecto_repos)
   end
--- backend/render-entrypoint.sh
+++ backend/render-entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "--- Running database migrations..."
 /app/bin/message_app eval "MessageApp.Release.migrate()"
 
+echo "--- Seeding database..."
+/app/bin/message_app eval "MessageApp.Release.seed()"
+
 echo "--- Starting Phoenix server..."
 exec /app/bin/message_app start
